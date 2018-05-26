a973837cacceccfc2b7e1cdb0cd838f88043dfd9
ROSENIO-PC\kenio <kenio3d@gmail.com>
2018-05-26 02:28:01 -0300

    Fix load caches right after export caches

--- scripts/Tools/Import_Cache.py
+++ scripts/Tools/Import_Cache.py
@@ -1,7 +1,7 @@
 #===============================================================================
 # Autor: Rosenio Pinto
 # e-mail: kenio3d@gmail.com
-# 
+#
 # This code need to be cleaned
 #===============================================================================
 from SystemConfig import *
@@ -14,95 +14,95 @@ class ImportCache(QWidget):
     def __init__(self, parent=None):
         super(ImportCache, self).__init__(parent)
         #=======================================================================
-        # #Parent widget under Maya main window        
+        # #Parent widget under Maya main window
         #=======================================================================
-        self.setParent(get_main_window())        
+        self.setParent(get_main_window())
         self.setWindowFlags(Qt.Window)
-        
+
         self.setGeometry(0, 0, 300, 300)
-        
+
         #=======================================================================
         # #Set the object name
         #=======================================================================
-        self.setObjectName('ImportCacheWindow_uniqueId')        
+        self.setObjectName('ImportCacheWindow_uniqueId')
         self.setWindowTitle('Import Cache')
-        
+
         self.current_category_tab = 0
         self.Import_Cache_Info    = {}
-        
-        
+
+
         self.initUI()
-        
-        
+
+
     #===========================================================================
     # #UI functions
     #===========================================================================
     def initUI(self):
         self.mainLayout            = QVBoxLayout(self)
         self.setLayout             = self.mainLayout
-        
+
         self.centralLayout         = QHBoxLayout(self)
         self.scenes_layout         = QVBoxLayout(self)
         self.scene_list_widget     = AW.AssetListWidget(tab_title='Cached Scenes', isStatic=True)
         self.CachedAssetWidgetsTab = AW.AssetListWidget()
-        
+
         self.scenes_widget = QWidget(self)
         self.scenes_widget.setLayout(self.scenes_layout)
-        
-        
+
+
         listWidgetSplitter = QSplitter(Qt.Horizontal)
         listWidgetSplitter.addWidget(self.scenes_widget)
         listWidgetSplitter.addWidget(self.CachedAssetWidgetsTab)
         listWidgetSplitter.setStyleSheet("QSplitter:handle{height: 16px; image: url(%sverticalHandle.png);}"%BATCH_io_IMAGES)
-        
+
         listWidgetSplitter.setSizes([400, 200])
-        
-        
+
+
         self.scenes_layout.addWidget(self.Add_Scene_Widget_HeaderUI())
         self.scenes_layout.addWidget(self.scene_list_widget)
         self.centralLayout.addWidget(listWidgetSplitter)
         self.mainLayout.addLayout(self.centralLayout)
-        
+
         self.scene_list_widget.SetFont('fontSize_H1', True)
-        
+
 
     def Add_Scene_Widget_HeaderUI(self):
             #===================================================================
             # #Add header scene widgets
             #===================================================================
             self.header_widget = AW.AssetWidget()
-            
+
             self.header_widget.SetCheckBox(True)
             self.header_widget.SetAssetLabel('scene name')
             self.header_widget.SetStatusLabel('status')
 
 
             self.header_widget.SetAssetBtn(u"\u25BC", self.print_none, True, True)
-            
+
             self.header_widget.SetAssetBtn(u"X", self.Remove_Selected, True, True)
 
-            
+
             self.header_widget.isTrigger    = False
             self.header_widget.isInterative = False
-            
-            
+
+
             checkbox_slot = self.Set_All_Scene_CheckState
             self.header_widget.SetCheckBoxChangeCmd(checkbox_slot)
-            
-            
+
+
             return self.header_widget
 
 
     def print_none(self):
         print 'None'
-        
-        
+
+
     def AddCachedSceneWidgetsUI(self, scene_short_name=''):
         self.scene_list_widget.CleanAssetWidgets()
-        
+
         Asset_DB = Set_DB()
         self.Asset_DB_Query = Asset_DB.query()
-        
+
         #=======================================================================
         # #Get all published scene names on db
         #=======================================================================
@@ -119,15 +119,15 @@ class ImportCache(QWidget):
             sceneWidget.SetAssetBtn(u"\u2699", partial(self.AddCachedAssetsUI, scene_short_name), True, False)
             sceneWidget.SetFont('fontSize_H3', True)
             sceneWidget.isTrigger = True
-            
+
             #===========================================================
             # #Set the checkbox change state function
             #===========================================================
             checkboxslot = partial(self.Set_Scene_CheckState, sceneWidget)
             sceneWidget.SetCheckBoxChangeCmd(checkboxslot)
-            
+
             self.scene_list_widget.AddAssetWidget(sceneWidget)
-            
+
 
 
     def AddCachedAssetsUI(self, scene_short_name):
@@ -140,38 +140,37 @@ class ImportCache(QWidget):
         Asset_DB   = Set_DB()
         sceneQuery = Asset_DB.query({'scene_short_name':scene_short_name})
 
-            
+
         if sceneQuery:
             for cache_list in sceneQuery:
                 assetDict = cache_list['reference_list']
-                
+
                 #===============================================================
                 # Put the cache list into the main cache dict to use later
                 #===============================================================
-                if not scene_short_name in self.Import_Cache_Info.keys():
-                    self.Import_Cache_Info[scene_short_name] = cache_list
-                
+                self.Import_Cache_Info[scene_short_name] = cache_list
+
                 #===============================================================
                 # Create a check state dict for references
                 #===============================================================
                 if not 'Reference_CheckState_Info' in self.Import_Cache_Info[scene_short_name].keys():
                     self.Import_Cache_Info[scene_short_name]['Reference_CheckState_Info'] = {}
-                
+
                 for cache_name, cache_list in assetDict.iteritems():
                     reference_type      = cache_list['reference_type']
                     reference_full_path = cache_list['cache_path']
-                    
+
                     #===========================================================
                     # #Populate check state dict
                     #===========================================================
                     check_state = True
                     if not reference_type in self.Import_Cache_Info[scene_short_name]['Reference_CheckState_Info'].keys():
                         self.Import_Cache_Info[scene_short_name]['Reference_CheckState_Info'][reference_type] = {cache_name:check_state}
-                        
+
                     if not cache_name in self.Import_Cache_Info[scene_short_name]['Reference_CheckState_Info'][reference_type].keys():
                         self.Import_Cache_Info[scene_short_name]['Reference_CheckState_Info'][reference_type][cache_name] = check_state
-                    
-                    
+
+
                     check_state = self.Import_Cache_Info[scene_short_name]['Reference_CheckState_Info'][reference_type][cache_name]
                     #===========================================================
                     # Create a tab for the proper category
@@ -185,20 +184,20 @@ class ImportCache(QWidget):
                     cache_widget.SetCheckBox(check_state)
                     cache_widget.SetAssetIcon(BATCH_io_IMAGES+'PipelineCache.png')
                     cache_widget.SetAssetLabel(cache_name)
-                    cache_widget.SetAssetBtn(u"\u25BC", partial(self.ReferenceAlembic, 
-                                                                    cache_list['cache_path'], 
-                                                                    sceneQuery['start_frame'], 
-                                                                    sceneQuery['end_frame'], 
-                                                                    cache_name), 
+                    cache_widget.SetAssetBtn(u"\u25BC", partial(self.ReferenceAlembic,
+                                                                    cache_list['cache_path'],
+                                                                    sceneQuery['start_frame'],
+                                                                    sceneQuery['end_frame'],
+                                                                    cache_name),
                                                                     True)
-                    
+
                     cache_widget.setToolTip('Scene___________'+scene_short_name+'\n'+
                                             'Path____________'+reference_full_path+'\n'+
                                             'Namespace_____'+cache_name+'\n'+
                                             'Type:___________'+reference_type
                                             )
-                    
-                    
+
+
                     cache_widget.SetFont('fontSize_H3', True)
                     cache_widget.isTrigger = True
 
@@ -207,7 +206,7 @@ class ImportCache(QWidget):
                     #===========================================================
                     checkboxslot = partial(self.Set_Cache_CheckState, scene_short_name, reference_type, cache_name)
                     cache_widget.SetCheckBoxChangeCmd(checkboxslot)
-                    
+
                     self.CachedAssetWidgetsTab.AddAssetWidget(cache_widget, True, reference_type)
 
                     self.Set_Cache_CheckState(scene_short_name, reference_type, cache_name, check_state)
@@ -217,7 +216,7 @@ class ImportCache(QWidget):
 
     def RemoveAlembic(self, scene_short_name, scene_widget):
         result = self.Confirm_Dialog()
-        
+
         if result == QMessageBox.Ok:
             Asset_DB   = Set_DB()
             sceneQuery = Asset_DB.query({'scene_short_name':scene_short_name})
@@ -232,37 +231,37 @@ class ImportCache(QWidget):
                                 # #Delete the alembic file
                                 #===================================================
                                 os.remove(cachePath)
-                                
+
                                 #===================================================
                                 # #Update the UI
                                 #===================================================
                                 scene_widget.close()
                                 self.CleanCachedAssetWidgetUI()
-                                
+
                                 #===================================================
                                 # #Update the DB
                                 #===================================================
                                 Asset_DB.cleanAll(sceneQuery['_id'])
                                 self.Import_Cache_Info[scene_short_name] = None
-                                
+
                                 #===================================================
                                 # log
                                 #===================================================
                                 print 'The cache: [ %s ] from scene: >> %s << was removed.'%(cachePath.split('scenes')[-1], scene_short_name)
-                                
+
                             except Exception, e:
                                 'This cache cannot be removed. Maybe in use.'
                                 print e
-    
-    
+
+
     def Remove_Selected(self):
         for scene_short_name in self.Import_Cache_Info.keys():
             if self.Import_Cache_Info[scene_short_name]:
                 scene_widget = self.scene_list_widget.assetWidgets[scene_short_name]
                 self.RemoveAlembic(scene_short_name, scene_widget)
-                
-            
-    
+
+
+
     def Confirm_Dialog(self):
         msgBox = QMessageBox()
         msgBox.setText("This action is undoable!")
@@ -272,7 +271,7 @@ class ImportCache(QWidget):
         msgBox.setIcon(QMessageBox.Warning)
         result = msgBox.exec_()
         return result
-    
+
 
     def CleanCachedAssetWidgetUI(self):
         self.current_category_tab = self.CachedAssetWidgetsTab.currentIndex()
@@ -300,8 +299,8 @@ class ImportCache(QWidget):
     def Set_All_Scene_CheckState(self, *args):
         for scene_widget in self.scene_list_widget.assetWidgets.values():
             scene_widget.Set_CheckState_Value(args[0])
-            
-    
+
+
     def Set_Cache_CheckState(self, *args):
         scene_short_name  = args[0]
         reference_type    = args[1]
@@ -311,13 +310,13 @@ class ImportCache(QWidget):
             checkState = False
         else:
             checkState = True
-            
+
         if not checkState:
             print scene_short_name, reference_type, reference, "Will not be considered on the batch process."
 
-        self.Import_Cache_Info[scene_short_name]['Reference_CheckState_Info'][reference_type][reference] = checkState    
+        self.Import_Cache_Info[scene_short_name]['Reference_CheckState_Info'][reference_type][reference] = checkState
 
-        
+
     #===========================================================================
     # #System
     #===========================================================================
@@ -329,11 +328,11 @@ class ImportCache(QWidget):
         #=======================================================================
         uName = cache_path.split(Get_Project())[-1]
         refNode = cmds.referenceQuery(cache_path, referenceNode=True)
-        
+
         cmds.file(uName, loadReference=refNode)
-        
+
         return cmds.referenceQuery(refNode, namespace=True)
-        
+
 
     def ReferenceAllAlembic(self, scene_short_name):
         #=======================================================================
@@ -346,14 +345,14 @@ class ImportCache(QWidget):
             for assetItem in assetDict.values():
                 namespace_name = assetItem['root_name'].split(':')[0]
                 reference_type = assetItem['reference_type']
-                
+
                 if Asset_DB_Query['Reference_CheckState_Info'][reference_type][namespace_name]:
-                
+
                     namespace_name = self.ReferenceAlembic(assetItem['cache_path'], Asset_DB_Query['start_frame'], Asset_DB_Query['end_frame'], namespace_name)
 
                     if not cmds.objExists(assetItem['root_name']):
                         alembicNode = cmds.ls('%s:*'%namespace_name, type='AlembicNode')
-                        
+
                         if alembicNode:
                             rootNodes   = cmds.listConnections(alembicNode)
                         else:
@@ -365,8 +364,8 @@ class ImportCache(QWidget):
                             except Exception, e:
                                 print e
                                 print "Problems to group alembic root nodes."
-                    
-                        
+
+
         self.SetOffset()
 
 
@@ -375,15 +374,15 @@ class ImportCache(QWidget):
     #===========================================================================
     def SetOffset(self):
         alembic_nodes = cmds.ls(type='AlembicNode')
-        
+
         for alembic_node in alembic_nodes:
             start_frame = cmds.getAttr(alembic_node+'.startFrame')
             end_frame   = cmds.getAttr(alembic_node+'.endFrame')
-            
+
             offset = (end_frame - start_frame)+1
-            
+
             cmds.setAttr(alembic_node+'.offset', -(start_frame-1))
-        
+
         self.SetTimeSlider(1, offset)
 
 
@@ -393,9 +392,3 @@ class ImportCache(QWidget):
     def SetTimeSlider(self, start_frame=0, end_frame=10):
         cmds.playbackOptions(min=start_frame, max=end_frame)
         cmds.playbackOptions(ast=start_frame, aet=end_frame)
-        
-        
-        
-        
-        
-        
